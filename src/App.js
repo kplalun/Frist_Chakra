b494180888eba56e700b6a32505227f844795bb2
Pokdeng <kp.lalun@gmail.com>
2022-09-20 16:51:30 +0700

    Fix

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
-import DisplayJohn , {JohnProfile} from "./Components/DisplayJohn"
-import {
-  ChakraProvider,
-  Button,
-  ButtonGroup,
-  Input,
-  Spinner,
-} from "@chakra-ui/react";
+import DisplayJohn, { JohnProfile } from "./Components/DisplayJohn";
+import { Button, Input, Spinner } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <ChakraProvider>
-        <div className="Botton">
-          <Button colorScheme="teal" size="lg">
-            Button
-          </Button>
-          <Button colorScheme="cyan" size="sm">
-            Button
-          </Button>
-          <Button colorScheme="orange" size="md">
-            Button
-          </Button>
-        </div>
+      <div className="Botton">
+        <Button colorScheme="teal" size="lg">
+          Button
+        </Button>
+        <Button colorScheme="cyan" size="sm">
+          Button
+        </Button>
+        <Button colorScheme="orange" size="md">
+          Button
+        </Button>
+      </div>
 
-        <div className="Spinner">
-          <Spinner color="red.500" size="xl" />
-          <Spinner color="green.100" size="sm" />
-          <Spinner color="yellow.500" size="md" />
-        </div>
+      <div className="Spinner">
+        <Spinner color="red.500" size="xl" />
+        <Spinner color="green.100" size="sm" />
+        <Spinner color="yellow.500" size="md" />
+      </div>
 
-        <div className="input">
-          <Input placeholder="extra small size" size="xs" />
-          <Input placeholder="small size" size="sm" />
-          <Input placeholder="medium size" size="md" />
-        </div>
-      </ChakraProvider>
+      <div className="input">
+        <Input placeholder="extra small size" size="xs" />
+        <Input placeholder="small size" size="sm" />
+        <Input placeholder="medium size" size="md" />
+      </div>
 
       <DisplayJohn />
       <JohnProfile />
