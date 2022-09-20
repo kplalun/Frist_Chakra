6084a0f8f986264646707e6336250eeea10af308
Pokdeng <kp.lalun@gmail.com>
2022-09-20 14:23:17 +0700

    Frist Charkra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DisplayJohn , {JohnProfile} from "./Components/DisplayJohn"
+import {
+  ChakraProvider,
+  Button,
+  ButtonGroup,
+  Input,
+  Spinner,
+} from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChakraProvider>
+        <div className="Botton">
+          <Button colorScheme="teal" size="lg">
+            Button
+          </Button>
+          <Button colorScheme="cyan" size="sm">
+            Button
+          </Button>
+          <Button colorScheme="orange" size="md">
+            Button
+          </Button>
+        </div>
+
+        <div className="Spinner">
+          <Spinner color="red.500" size="xl" />
+          <Spinner color="green.100" size="sm" />
+          <Spinner color="yellow.500" size="md" />
+        </div>
+
+        <div className="input">
+          <Input placeholder="extra small size" size="xs" />
+          <Input placeholder="small size" size="sm" />
+          <Input placeholder="medium size" size="md" />
+        </div>
+      </ChakraProvider>
+
+      <DisplayJohn />
+      <JohnProfile />
+    </>
   );
 }
 
